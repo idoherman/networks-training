02e1bf47299014b2fc28a42afdda416c047b5277
idoherman <ido27000@gmail.com>
2022-08-09 00:00:17 +0300

    done part 2.6.3

--- networks_part_2.6.2-3/server.py
+++ networks_part_2.6.2-3/server.py
@@ -3,57 +3,68 @@ import datetime
 import random
 
 
-def send_time(client_socket):
-    now = datetime.datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    reply = "My time here is: " + current_time + " :)"
+def send_reply(client_socket, reply):
     length_msg = len(reply)
     length_msg = str(length_msg).zfill(2)
     reply = str(length_msg) + reply
     client_socket.send(reply.encode())
+
+
+def send_time(client_socket):
+    now = datetime.datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    reply = "My time here is: " + current_time + " :)"
+    send_reply(client_socket, reply)
 
 
 def send_name(client_socket):
     name = "Ido's Server"
     reply = "The server name is: " + name + " :)"
-    length_msg = len(reply)
-    length_msg = str(length_msg).zfill(2)
-    reply = str(length_msg) + reply
-    client_socket.send(reply.encode())
+    send_reply(client_socket, reply)
 
 
 def send_random_num(client_socket):
     num = random.randint(0, 10)
     reply = f"My random num is:  {num} :)"
-    length_msg = len(reply)
-    length_msg = str(length_msg).zfill(2)
-    reply = str(length_msg) + reply
-    client_socket.send(reply.encode())
+    send_reply(client_socket, reply)
 
 
 def send_no_request(client_socket, client_msg):
     reply = f"'{client_msg}' is not an existing request"
-    length_msg = len(reply)
-    length_msg = str(length_msg).zfill(2)
-    reply = str(length_msg) + reply
-    client_socket.send(reply.encode())
+    send_reply(client_socket, reply)
+
+
+def send_no_length(client_socket):
+    reply = "You need to send the length first :("
+    send_reply(client_socket, reply)
 
 
 def get_send_msg(client_socket):
-    length_client_msg = int(client_socket.recv(2).decode())
+    length_client_msg = client_socket.recv(2).decode()
+
+    if not length_client_msg.isnumeric():
+        send_no_length(client_socket)
+        return
+
+    length_client_msg = int(length_client_msg)
     client_msg = client_socket.recv(length_client_msg).decode()
     print("The client sent the message: " + client_msg)
+
     if client_msg.upper() == "EXIT":
         return True
+
     elif client_msg.upper() == "TIME":
         send_time(client_socket)
         return False
+
     elif client_msg.upper() == "WHORU":
         send_name(client_socket)
         return False
+
     elif client_msg.upper() == "RAND":
         send_random_num(client_socket)
         return False
+
     else:
         send_no_request(client_socket, client_msg)
 
